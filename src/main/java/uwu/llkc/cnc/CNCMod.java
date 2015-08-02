61c3b51792893707cfa613f5efc3f88274b5b441
cpw <cpw@weeksfamily.ca>
2015-08-02 00:03:47 -0400

    setup 1.21 mdk and basics

---
+++ src/main/java/uwu/llkc/cnc/CNCMod.java
@@ -0,0 +1,14 @@
+package uwu.llkc.cnc;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(CNCMod.MOD_ID)
+public class CNCMod {
+    public static final String MOD_ID = "cnc";
+
+    public CNCMod(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
