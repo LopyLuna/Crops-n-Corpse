81a712036e53dc3a02e3912a44e7e7f1bf646a3b
lvgraef <lvgraef@zeelandnet.nl>
2024-08-01 21:29:22 +0200

    Fixed loading problems

--- src/main/java/uwu/llkc/cnc/common/events/ModEvents.java
+++ src/main/java/uwu/llkc/cnc/common/events/ModEvents.java
@@ -1,50 +1,16 @@
 package uwu.llkc.cnc.common.events;
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import net.minecraft.world.entity.npc.VillagerProfession;
-import net.minecraft.world.entity.npc.VillagerTrades;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.trading.ItemCost;
-import net.minecraft.world.item.trading.MerchantOffer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
-import net.neoforged.neoforge.event.village.VillagerTradesEvent;
-import net.neoforged.neoforge.event.village.WandererTradesEvent;
 import uwu.llkc.cnc.CNCMod;
 import uwu.llkc.cnc.common.entities.plants.Peashooter;
 import uwu.llkc.cnc.common.init.EntityTypeRegistry;
-import uwu.llkc.cnc.common.init.ItemRegistry;
-
-import java.util.List;
 
 @EventBusSubscriber(modid = CNCMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class ModEvents {
     @SubscribeEvent
     public static void entityAttributeCreation(final EntityAttributeCreationEvent event) {
         event.put(EntityTypeRegistry.PEASHOOTER.get(), Peashooter.attributes().build());
-    }
-
-    @SubscribeEvent
-    public static void addCustomTrades(final VillagerTradesEvent event) {
-        if(event.getType() == VillagerProfession.FARMER) {
-            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
-
-            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemCost(ItemRegistry.PEA_POD.get(), pRandom.nextIntBetweenInclusive(12, 17)),
-                    new ItemStack(Items.EMERALD, 1),
-                    10, 8, 0.02f));
-        }
-    }
-
-    @SubscribeEvent
-    public static void addCustomWanderingTrades(WandererTradesEvent event) {
-        List<VillagerTrades.ItemListing> genericTrades = event.getGenericTrades();
-
-        genericTrades.add((pTrader, pRandom) -> new MerchantOffer(
-                new ItemCost(Items.EMERALD, 1),
-                new ItemStack(ItemRegistry.PEA_POD.get(), 3),
-                20, 2, 0.1f));
     }
 }
--- src/main/java/uwu/llkc/cnc/common/init/ItemRegistry.java
+++ src/main/java/uwu/llkc/cnc/common/init/ItemRegistry.java
@@ -3,22 +3,20 @@ package uwu.llkc.cnc.common.init;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import uwu.llkc.cnc.CNCMod;
 import uwu.llkc.cnc.common.items.SeedPacketItem;
 
-import java.util.function.Supplier;
-
 public class ItemRegistry {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CNCMod.MOD_ID);
 
-    public static final DeferredHolder<Item, SeedPacketItem> SEED_PACKET = ITEMS.registerItem("seed_packet", SeedPacketItem::new);
-    public static final Supplier<Item> PLANT_FOOD = ITEMS.registerSimpleItem("plant_food");
-    public static final Supplier<Item> SUN = ITEMS.registerSimpleItem("sun");
-    public static final DeferredHolder<Item, BlockItem> RAW_PEA = ITEMS.registerSimpleBlockItem("raw_pea", BlockRegistry.PEASHOOTER_CROP, new Item.Properties().food(Foods.RAW_PEA));
-    public static final Supplier<Item> COOKED_PEA = ITEMS.registerSimpleItem("cooked_pea", new Item.Properties().food(Foods.COOKED_PEA));
-    public static final DeferredHolder<Item, Item> PEA_POD = ITEMS.registerSimpleItem("pea_pod", new Item.Properties().food(Foods.PEA_POD));
+    public static final DeferredItem<Item> SEED_PACKET = ITEMS.registerItem("seed_packet", SeedPacketItem::new);
+    public static final DeferredItem<Item> PLANT_FOOD = ITEMS.registerSimpleItem("plant_food");
+    public static final DeferredItem<Item> SUN = ITEMS.registerSimpleItem("sun");
+    public static final DeferredItem<BlockItem> RAW_PEA = ITEMS.registerSimpleBlockItem("raw_pea", BlockRegistry.PEASHOOTER_CROP, new Item.Properties().food(Foods.RAW_PEA));
+    public static final DeferredItem<Item> COOKED_PEA = ITEMS.registerSimpleItem("cooked_pea", new Item.Properties().food(Foods.COOKED_PEA));
+    public static final DeferredItem<Item> PEA_POD = ITEMS.registerSimpleItem("pea_pod", new Item.Properties().food(Foods.PEA_POD));
 
     static class Foods {
         public static final FoodProperties PEA_POD = new FoodProperties.Builder().nutrition(9).saturationModifier(0.5f).build();
