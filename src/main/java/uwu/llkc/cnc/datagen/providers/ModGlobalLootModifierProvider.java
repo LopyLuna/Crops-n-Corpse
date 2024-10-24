56e2748cf7bb345b245bbc63de1e2474d112ed55
lvgraef <lvgraef@zeelandnet.nl>
2024-10-24 23:16:47 +0200

    fixed block drop issue

--- src/main/java/uwu/llkc/cnc/datagen/providers/ModGlobalLootModifierProvider.java
+++ src/main/java/uwu/llkc/cnc/datagen/providers/ModGlobalLootModifierProvider.java
@@ -2,10 +2,12 @@ package uwu.llkc.cnc.datagen.providers;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.neoforged.neoforge.common.data.GlobalLootModifierProvider;
 import net.neoforged.neoforge.common.loot.AddTableLootModifier;
+import net.neoforged.neoforge.common.loot.LootTableIdCondition;
 import uwu.llkc.cnc.CNCMod;
 
 import java.util.concurrent.CompletableFuture;
@@ -20,7 +22,8 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
         add(
                 "add_cherry_to_leaves",
                 new AddTableLootModifier(new LootItemCondition[]{
-                        LootItemRandomChanceCondition.randomChance(0.2f).build()
+                        LootItemRandomChanceCondition.randomChance(0.2f).build(),
+                        LootTableIdCondition.builder(ResourceLocation.withDefaultNamespace("cherry_leaves")).build()
                 }, ModLootTableSubProvider.CHERRIES_DROP)
         );
     }
