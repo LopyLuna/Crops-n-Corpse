613c07330d92b002a7be6f7d933b89662fdadc4e
lvgraef <lvgraef@zeelandnet.nl>
2024-10-24 16:03:35 +0200

    Potatomine explosion isnt a mob explosion

--- src/main/java/uwu/llkc/cnc/common/entities/plants/PotatoMine.java
+++ src/main/java/uwu/llkc/cnc/common/entities/plants/PotatoMine.java
@@ -136,7 +136,7 @@ public class PotatoMine extends CNCPlant {
                 }
                 return super.shouldDamageEntity(explosion, entity);
             }
-        }, getX(), getY(), getZ(), 1.5f, false, Level.ExplosionInteraction.NONE);
+        }, getX(), getY(), getZ(), 1.5f, false, Level.ExplosionInteraction.MOB);
         this.remove(RemovalReason.KILLED);
     }
 
