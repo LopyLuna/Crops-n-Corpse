309fc7ad0e1ad25554696faea4b987c58a1aec6b
lvgraef <86008039+itskillerluc@users.noreply.github.com>
2025-01-21 22:36:52 +0100

    fixed wrong model part being referenced

--- src/main/java/uwu/llkc/cnc/client/util/ClientProxy.java
+++ src/main/java/uwu/llkc/cnc/client/util/ClientProxy.java
@@ -70,7 +70,7 @@ public class ClientProxy {
     }
 
     public static void createFootSoldierArm(FootSoldier footSoldier) {
-        var model = FootSoldierModel.createBodyLayer().bakeRoot().getChild("left_arm").getChild("ForeArm");
+        var model = FootSoldierModel.createBodyLayer().bakeRoot().getChild("left_arm").getChild("forearm");
         Minecraft.getInstance().particleEngine.add(new PhysicsModelParticle(((ClientLevel) footSoldier.level()), footSoldier.getX(), footSoldier.getY() + 1.25, footSoldier.getZ(), Either.left(model), poseStack -> {
             poseStack.translate(Vec3.directionFromRotation(0, footSoldier.getYRot()).z * .32, 0, Vec3.directionFromRotation(0, footSoldier.getYRot()).x * -.32);
             poseStack.mulPose(Axis.YP.rotationDegrees(-footSoldier.getVisualRotationYInDegrees()));
