4d162584b7129b5873fa807c54eefed7afcbfc3d
lvgraef <86008039+itskillerluc@users.noreply.github.com>
2025-05-27 22:43:43 +0200

    added helmets

--- src/main/java/uwu/llkc/cnc/common/items/ZombieVariantProperty.java
+++ src/main/java/uwu/llkc/cnc/common/items/ZombieVariantProperty.java
@@ -6,7 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import uwu.llkc.cnc.CNCMod;
-import uwu.llkc.cnc.common.entities.zombies.Browncoat;
+import uwu.llkc.cnc.common.entities.zombies.MummifiedBrowncoat;
 
 import javax.annotation.Nullable;
 
@@ -20,7 +20,7 @@ public class ZombieVariantProperty implements ClampedItemPropertyFunction {
     @Override
     public float unclampedCall(ItemStack stack, @Nullable ClientLevel level, @Nullable LivingEntity entity, int seed) {
         return switch (entity) {
-            case Browncoat browncoat -> 0.1f;
+            case MummifiedBrowncoat bc -> 0.1f;
             case null, default -> 0.0f;
         };
     }
