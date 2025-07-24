fb9a7cf324338e22284c1297655a9e59a51a74bf
lvgraef <86008039+itskillerluc@users.noreply.github.com>
2025-07-25 01:17:26 +0300

    fixed mummified browncoat

--- src/main/java/uwu/llkc/cnc/client/renderers/entity/MummifiedBrowncoatRenderer.java
+++ src/main/java/uwu/llkc/cnc/client/renderers/entity/MummifiedBrowncoatRenderer.java
@@ -1,7 +1,9 @@
 package uwu.llkc.cnc.client.renderers.entity;
 
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
+import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import uwu.llkc.cnc.CNCMod;
@@ -9,12 +11,14 @@ import uwu.llkc.cnc.client.models.entity.MummifiedBrowncoatModel;
 import uwu.llkc.cnc.client.models.entity.SandstormLayer;
 import uwu.llkc.cnc.common.entities.zombies.MummifiedBrowncoat;
 
-public class MummifiedBrowncoatRenderer extends HumanoidMobRenderer<MummifiedBrowncoat, MummifiedBrowncoatModel<MummifiedBrowncoat>> {
+public class MummifiedBrowncoatRenderer extends MobRenderer<MummifiedBrowncoat, MummifiedBrowncoatModel<MummifiedBrowncoat>> {
     public static final ResourceLocation TEXTURE = CNCMod.rl("textures/entity/mummified_browncoat.png");
 
     public MummifiedBrowncoatRenderer(EntityRendererProvider.Context context) {
         super(context, new MummifiedBrowncoatModel<>(context.bakeLayer(MummifiedBrowncoatModel.MAIN_LAYER)), 0.5f);
         addLayer(new SandstormLayer<>(context, this, (browncoat) -> browncoat.getEntityData().get(MummifiedBrowncoat.HAS_SANDSTORM)));
+        addLayer(new CustomHeadLayer<>(this, context.getModelSet(), 1, 1, 1, context.getItemInHandRenderer()));
+        addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()));
     }
 
     @Override
